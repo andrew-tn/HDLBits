0525bf827673cd98aa10ad6b3a1fcb347c758585
andrew-tn <112051065+andrew-tn@users.noreply.github.com>
2022-08-24 22:22:47 -0700

    Create three modules.v

---
+++ Verilog Language/Modules/three modules.v
@@ -0,0 +1,12 @@
+// https://hdlbits.01xz.net/wiki/Module_shift
+module top_module ( input clk, input d, output q );
+    
+    wire out1, out2;
+    
+    my_dff inst1(clk, d, out1);
+    
+    my_dff inst2(clk, out1, out2);
+    
+    my_dff inst3(clk, out2, q);
+
+endmodule
